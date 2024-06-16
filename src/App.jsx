394f5bcecc4032e266daab55309aa085293d92c8
Emmanuel <manumtz20@gmail.com>
2024-06-16 02:18:08 -0600

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>new project</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // aplicara las clases de tailwind a los archivos con estas exetenciones
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
