410a289e179998244fd478403585694ae0221f3f
Emmanuel <manumtz20@gmail.com>
2024-06-18 04:02:19 -0600

    carga de datos al refrescar la pagina

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ import { addTasks, userSave } from '../redux/actions';
 import TYPE_OF_USERS from '../constants/typeOfUsers';
 import TaskTableAdmin from '../components/TaskTableAdmin';
 import TaskTableTec from '../components/TaskTableTec';
+import taskService from '../services/taskService';
 
 const HomePage = () => {
   const user = useSelector((state) => state.user);
@@ -16,11 +17,21 @@ const HomePage = () => {
 
   // volver a cargar los datos si se refresca la pagina
   useEffect(() => {
-    const user = localStorageService.getUser();
-    const tasks = localStorageService.getTasks();
+    const token = localStorageService.getToken();
 
-    dispatch(userSave(user));
-    dispatch(addTasks(tasks));
+    if (token) {
+      const user = localStorageService.getUser();
+      dispatch(userSave(user));
+
+      taskService
+        .getTasksRequest(token)
+        .then(({ data }) => {
+          dispatch(addTasks(data));
+        })
+        .catch(() => {
+          window.alert('Error al cargar las Tareas');
+        });
+    }
   }, [dispatch]);
 
   return (
@@ -33,7 +44,9 @@ const HomePage = () => {
 
       <div className="px-14 flex justify-between">
         <h3 className="text-2xl mt-10">Tareas pendientes</h3>
-        <NavButton path={PATH_ROUTES.CREATE_TASK} name={'Nueva Tarea'} />
+        {user.typeOfUser === TYPE_OF_USERS.AREA && (
+          <NavButton path={PATH_ROUTES.CREATE_TASK} name={'Nueva Tarea'} />
+        )}
       </div>
       {user.typeOfUser === TYPE_OF_USERS.AREA && <TaskTable />}
       {user.typeOfUser === TYPE_OF_USERS.ADMIN && <TaskTableAdmin />}
